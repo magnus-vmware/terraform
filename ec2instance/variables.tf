b25693abb4b1397a429410dcb20e5c10df008ccf
magnus-vmware <mbergman@vmware.com>
2020-09-11 09:51:19 +0200

    Update variables.tf
    Removed authentication keys

--- ec2instance/variables.tf
+++ ec2instance/variables.tf
@@ -1,11 +1,3 @@
-variable "access_key" {
-  type = string
-  description = "Access Key"
-}
-variable "secret_key" {
-  type = string
-  description = "Secret Key"
-}
 variable "instance_name" {
   type = string
   description = "EC2 instance name"
