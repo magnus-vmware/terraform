2f549d09842181145210dc81e8118a11deded9e3
magnus-vmware <mbergman@vmware.com>
2021-01-22 13:56:07 +0100

    Update Google instance file

--- google-instance/main.tf
+++ google-instance/main.tf
@@ -1,25 +1,30 @@
-terraform {
-  required_providers {
-    vra = {
-      source = "vmware/vra"
-      version = "0.3.2"
-    }
+// Configure the Google Cloud provider
+provider "google" {
+ project     = "magnus-bergman"
+ region      = "us-west1"
+}
+
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ name         = "flask-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-west1-a"
+
+   boot_disk {
+     initialize_params {
+       image = "debian-cloud/debian-9"
+     }
+   }
+   network_interface {
+     network = "default"
+
+     access_config {
+       // Include this section to give the VM an external ip address
+     }
   }
-}
-    
-provider "vra" {
-    url = var.url
-    refresh_token = var.refresh_token
-}
-
-data vra_project "myproject" {
-    name = "Dev"
-}
-
-resource "vra_deployment" "mydeployment" {
-    name = "MyDeployment"
-    description = "This is my stuff"
-    blueprint_id = "89f4240a-bf13-4c5a-8131-98c47352ca3e"
-    blueprint_version = ""
-    project_id = "4866a530-eccd-44f1-af99-ee2cf6c1f81b"
 }
